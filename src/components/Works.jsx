d8ea8b0b10d22ec0084511a89dfed21e72076eca
Hemant <hemr123@gmail.com>
2023-04-04 12:56:23 +0200

    Initial Commit

---
+++ src/components/Works.jsx
@@ -0,0 +1,175 @@
+import React from 'react';
+import code from '../assets/code2.png';
+import chip8 from '../assets/Chip-8.gif';
+import Tron from '../assets/Tron.jpeg';
+import Raytracer from '../assets/portfolio2.jpg';
+import Rasterizer from '../assets/DualRastizer.gif';
+import ExtendoBrawl from '../assets/Extendo.png';
+import Heaven3D from '../assets/Heaven-3D.png';
+
+const Works = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8 w-full flex justify-center items-center flex-col'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+            Work
+          </p>
+          <p className='py-6 text-2xl'>Check out some of my most recent work</p>
+        </div>
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            {/* Grid Item */}
+
+          <div
+            style={{ backgroundImage: `url(${chip8})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Chip-8 Emulator
+              </span>
+              <p className='text-center'>A Chip-8 Emulator built with C++ Visual studio 2022 and SDL as graphics renderer.</p>
+              <div className='pt-8 text-center'>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://github.com/Hemant-Gurung/CHIP-8'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Tron})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                TRON
+              </span>
+              <p className='text-center'>A Tron 2D arcade game built with self made 2D Game engine in C++ and using SDL as a graphics renderer.</p>
+              <div className='pt-8 text-center'>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://github.com/Hemant-Gurung/Manaengine'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Raytracer})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Raytracer
+              </span>
+              <p className='text-center'>A Raytracer built with Visual studio c++ with custom engine.</p>
+              <div className='pt-8 text-center'>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://github.com/Hemant-Gurung/RayTracing'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Rasterizer})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Rasterizer
+              </span>
+              <p className='text-center'>A Rasterizer made with visual studio C++ using custom engine.</p>
+              <div className='pt-8 text-center'>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://github.com/Hemant-Gurung/Dual-Rasterizer'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${ExtendoBrawl})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Extendo-Brawl
+              </span>
+              <p className='text-center'>Extendo-Brawl local multiplayer game built with Unity Engine.</p>
+              <div className='pt-8 text-center'>
+                <a href='https://lars-smet.itch.io/group29extendo-brawl'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Heaven3D})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Heaven-3D
+              </span>
+              <p className='text-center'>It is a Heven-3D game engine made with C++. It is an ongoing project..</p>
+              <div className='pt-8 text-center'>
+                {/* <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://github.com/Hemant-Gurung/HEAVEN3D-'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Works;
